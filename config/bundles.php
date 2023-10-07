35e17e571514ad5c0674df3a5040982a0c7e37ef
gregdev <gvandevray@numericable.fr>
2023-10-07 21:43:15 +0200

    update doctrine to 2.0

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
