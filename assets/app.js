6bae28ec7d270753e0174f900b2deeb7432edec1
gregdev <gvandevray@numericable.fr>
2023-10-27 13:02:28 +0200

    formatting homepage + fix bug responsive header

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,16 @@
 // any CSS you import will output into a single css file (app.css in this case)
 require('bootstrap');
 import './styles/app.css';
+
+const buttonSectionCreate = document.getElementById('btn-section-create');
+const buttonSectionTodo = document.getElementById('btn-section-todo');
+const buttonSectionFinished = document.getElementById('btn-section-finished');
+buttonSectionCreate?.addEventListener("click", function() {
+    window.location.href = buttonSectionCreate.getAttribute("data-url");
+});
+buttonSectionTodo?.addEventListener("click", function() {
+    window.location.href = buttonSectionTodo.getAttribute("data-url");
+});
+buttonSectionFinished?.addEventListener("click", function() {
+    window.location.href = buttonSectionFinished.getAttribute("data-url");
+});
